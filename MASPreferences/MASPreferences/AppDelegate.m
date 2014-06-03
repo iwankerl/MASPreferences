001d0f76a1573521295528dc3e9d2d89661fca54
Isaac Wankerl <isaac@iwankerl.com>
2014-06-02 20:35:57 -0500

    Adding a test app project

---
+++ MASPreferences/MASPreferences/AppDelegate.m
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.m
+//  MASPreferences
+//
+//  Created by Isaac Wankerl on 5/29/14.
+//  Copyright (c) 2014 Kerlmax LLC. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+	// Insert code here to initialize your application
+}
+
+@end
---
+++ MASPreferences/MASPreferences/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  MASPreferences
+//
+//  Created by Isaac Wankerl on 5/29/14.
+//  Copyright (c) 2014 Kerlmax LLC. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[])
+{
+	return NSApplicationMain(argc, argv);
+}
